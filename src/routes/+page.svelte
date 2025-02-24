f0ab41ebd2ea0a6aa877fa3c6213f293d7a2d010
Jasmin Liao <jgliao@ucsc.edu>
2025-02-24 15:30:41 -0800

    "added stuff to the webpage"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+	let name = 'Svelte';
+    let src = '/tutorial/image.gif';
+	let name1 = 'Rick Astley';
+</script>
+
+<h1>Hello world!</h1>
+
+<h1>Hello {name.toUpperCase()}! </h1>
+
+
+<img src={src} alt="A {name1} dances." />
